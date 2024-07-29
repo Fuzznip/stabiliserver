e9ead3defcbd3ad7640b1cb655e6c0f09a3f8a31
funzip <19338376+Fuzznip@users.noreply.github.com>
2024-07-29 16:17:00 -0700

    Manual submit (#21)
    * mostly good, just need to fix webhook request malformat
    
    * simple embed fix

--- routes/bot_submission_route.py
+++ routes/bot_submission_route.py
@@ -16,34 +16,50 @@ def handle_request():
   if data is None:
     return jsonify({ "error": "Invalid request" })
 
-  outputs = submit(data["user"], format(data["discordId"], '.0f'), data["source"], data["item"], 0, 0, "MANUAL")
+  output = submit(data["user"], format(data["discordId"], '.0f'), data["source"], data["item"], 0, 0, "MANUAL")
+  screenshotItems = {}
+  if output is not None:
+    threadIds = output["threadList"]
+    for threadId in threadIds:
+      if threadId not in screenshotItems:
+        screenshotItems[threadId] = []
+      if "message" in output:
+        screenshotItems[threadId].append(output["message"])
+      else:
+        screenshotItems[threadId].append(data["item"])
 
-  for output in outputs:
-    url = os.environ.get("WEBHOOK") + "?thread_id=" + output
-  
-    embeds = [
-      {
-        'author': {
-          'name': data["user"]
-        },
-        'description': data["item"],
-        'image': {
-          'url': data["attachment"]
+  print(screenshotItems)
+  if screenshotItems:
+    for threadId, itemList in screenshotItems.items():
+      # for each item in result, send a webhook
+      embeds = [
+        {
+          'author': {
+            'name': data['user'],
+          },
+          'description': '',
+          'image': {
+            'url': data['attachment']
+          }
         }
+      ]
+
+      # Join all items in itemList with a newline character separating them
+      embeds[0]['description'] = "\n".join(itemList)
+      print(embeds) 
+
+      payload = {
+        'embeds': embeds
       }
-    ]
 
-    payload = {
-      'embeds': embeds
-    }
+      payload_link = os.environ.get("WEBHOOK") + '?thread_id=' + threadId
+      result = requests.post(payload_link, data = {'payload_json': json.dumps(payload)})
 
-    result = requests.post(url, data = {'payload_json': json.dumps(payload)})
-
-    try:
-      result.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-      print(err)
+      try:
+        result.raise_for_status()
+      except requests.exceptions.HTTPError as err:
+        print(err)
   
-  if result:
-    return jsonify({ "message": "Submission received" })
-  return jsonify({ "message": "No action recorded" })
+  if screenshotItems:
+    return jsonify({ "message": "Submission received" }), 200
+  return jsonify({ "message": "No action recorded" }), 200

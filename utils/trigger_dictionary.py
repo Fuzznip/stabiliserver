b5f0a4cdab642c60404da807b72f76e6d928443a
Fuzznip <fuzznip@gmail.com>
2025-05-25 22:28:52 -0700

    Remove unused comments and streamline whitelist data assignment in set_whitelist_data function

--- utils/trigger_dictionary.py
+++ utils/trigger_dictionary.py
@@ -1,5 +1,3 @@
-# dropDictionary is a dictionary of pairs of items and drop sources to the list of channels they should post in
-# eg. { ("abyssal whip", "abyssal demon"): [ "1233130963870154864", "1232048319996625029", ... ] }
 class WhitelistData:
     triggers: list[tuple[str, str]] = []
     killCountTriggers: list[str] = []
@@ -18,5 +16,6 @@ def set_whitelist_data(newWhitelistData: WhitelistData) -> None:
     Sets the whitelist data.
     This is used to update the whitelist data when it is reloaded.
     """
-    global data
-    data = newWhitelistData
+    data.triggers = newWhitelistData.triggers
+    data.killCountTriggers = newWhitelistData.killCountTriggers
+    data.messageFilters = newWhitelistData.messageFilters

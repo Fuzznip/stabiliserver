a61bbfebbc782f7b69abe44f94a232e4d57f2c02
Fuzznip <fuzznip@gmail.com>
2026-02-05 09:21:44 -0800

    feat: adding chat whitelist stuff

--- utils/request_handlers/chat_handler.py
+++ utils/request_handlers/chat_handler.py
@@ -1,6 +1,20 @@
+from models.submission import Submission
+from utils.s3_upload import upload_to_s3
 from .parse_response import DiscordEmbedData
+from ..submit import write
 
-def parse_chat(data) -> list[tuple[str, DiscordEmbedData]]:
-    # Implement logic for handling 'CHAT' type
-    print(f"Parsing CHAT data: {data}")
-    return []
+def parse_chat(data: Submission, file: bytes | None = None) -> list[tuple[str, DiscordEmbedData]]:
+    img_path = None
+    if file:
+        img_path = upload_to_s3(file)
+
+    return write(
+        player=data.playerName,
+        discordId=data.discordUser.id if data.discordUser else "None",
+        trigger=data.extra.message,
+        source=data.extra.source,
+        quantity=1,
+        totalValue=0,
+        type="CHAT",
+        img_path=img_path
+    )

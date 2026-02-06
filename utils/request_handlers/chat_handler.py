dc68382e205cb63fbc0e865f61d4644ad5677bab
funzip <19338376+Fuzznip@users.noreply.github.com>
2026-02-06 12:17:55 -0800

    adds totem rummage hack for quantity (#36)

--- utils/request_handlers/chat_handler.py
+++ utils/request_handlers/chat_handler.py
@@ -3,6 +3,14 @@ from utils.s3_upload import upload_to_s3
 from .parse_response import DiscordEmbedData
 from ..submit import write
 
+def winter_bingo_2026_check_totem_quantity(trigger: str) -> int:
+    # Check if "offerings five times" is in the trigger
+    if "you rummage through the offerings five times" in trigger.lower():
+        return 5
+    elif "you rummage through the offerings" in trigger.lower():
+        return 1
+    return 1
+
 def parse_chat(data: Submission, file: bytes | None = None) -> list[tuple[str, DiscordEmbedData]]:
     img_path = None
     if file:
@@ -13,7 +21,7 @@ def parse_chat(data: Submission, file: bytes | None = None) -> list[tuple[str, D
         discordId=data.discordUser.id if data.discordUser else "None",
         trigger=data.extra.message,
         source=data.extra.source,
-        quantity=1,
+        quantity=winter_bingo_2026_check_totem_quantity(data.extra.message),
         totalValue=0,
         type="CHAT",
         img_path=img_path
